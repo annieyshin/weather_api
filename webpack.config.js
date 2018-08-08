62354c3c87c6d507db0fb5f584553c79bf3244ca
Annie Shin <annieyshin@gmail.com>
2018-08-08 14:11:00 -0700

    add in second API call using vanilla javascript

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/weather-interface.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Journal Assignment',
-      template: './src/index.html',
+      template: './src/weather.html',
       inject: 'body'
     }),
     new Dotenv()
@@ -33,7 +33,7 @@ module.exports = {
           'css-loader'
         ]
       },
-      {              
+      {
         test: /\.js$/,
         exclude: [
           /node_modules/,
