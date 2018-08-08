d6b1fdbf083489ea408840f250b96508635e5d36
Annie Shin <annieyshin@gmail.com>
2018-08-08 13:15:14 -0700

    initial commit, set up API call

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+{
+    "coord": {
+        "lon": -122.67,
+        "lat": 45.52
+    },
+    "weather": [
+        {
+            "id": 721,
+            "main": "Haze",
+            "description": "haze",
+            "icon": "50d"
+        },
+        {
+            "id": 711,
+            "main": "Smoke",
+            "description": "smoke",
+            "icon": "50d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 301.1,
+        "pressure": 1012,
+        "humidity": 39,
+        "temp_min": 298.15,
+        "temp_max": 303.15
+    },
+    "visibility": 16093,
+    "wind": {
+        "speed": 2.1,
+        "deg": 330
+    },
+    "clouds": {
+        "all": 90
+    },
+    "dt": 1533757980,
+    "sys": {
+        "type": 1,
+        "id": 2274,
+        "message": 0.0067,
+        "country": "US",
+        "sunrise": 1533733438,
+        "sunset": 1533785253
+    },
+    "id": 5746545,
+    "name": "Portland",
+    "cod": 200
+}
